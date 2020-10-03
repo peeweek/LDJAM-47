b4eaef8c97cc94e60808b50786611f6fdfdab763
Thomas ICHÉ <peeweek@gmail.com>
2020-10-03 08:46:00 +0200

    Build Level 1

---
+++ Packages/net.peeweek.retroizer/Editor/RetroizerEditor.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Retroizer
+{
+    [CustomEditor(typeof(Retroizer))]
+    public class RetroizerEditor : Editor
+    {
+        protected Dictionary<string, SerializedProperty> m_Properties;
+
+        private void OnEnable()
+        {
+            if (m_Properties == null)
+                m_Properties = new Dictionary<string, SerializedProperty>();
+
+            AddProperty("m_TargetCamera");
+            AddProperty("m_Resolution");
+            AddProperty("m_Orientation");
+            AddProperty("m_CustomWidth");
+            AddProperty("m_CustomHeight");
+            AddProperty("m_Quality");
+            AddProperty("OverscanMaskHardness");
+
+        }
+
+        private void AddProperty(string name)
+        {
+            var prop = serializedObject.FindProperty(name);
+            if (prop != null)
+                m_Properties.Add(name, prop);
+        }
+
+        private void OnDisable()
+        {
+            m_Properties.Clear();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(m_Properties["m_Resolution"], false);
+            if (m_Properties["m_Resolution"].intValue == (int)Retroizer.Resolution._Custom)
+            {
+                EditorGUILayout.PropertyField(m_Properties["m_CustomWidth"], false);
+                EditorGUILayout.PropertyField(m_Properties["m_CustomHeight"], false);
+            }
+            EditorGUILayout.PropertyField(m_Properties["m_Orientation"], false);
+            EditorGUILayout.PropertyField(m_Properties["m_TargetCamera"], false);
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Shader Properties", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(m_Properties["m_Quality"], false);
+            EditorGUILayout.PropertyField(m_Properties["OverscanMaskHardness"], false);
+
+            EditorGUI.indentLevel--;
+
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+                (serializedObject.targetObject as Retroizer).UpdateMaterial();
+            }
+        }
+    }
+
+}
